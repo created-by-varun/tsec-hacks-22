ca8ad1773a8420833bee059a19efae9eaeba05a0
punith.gowda <punith.gowda@gida.in>
2022-03-09 16:15:04 +0530

    doc patient UI

--- components/Doctor/Home.js
+++ components/Doctor/Home.js
@@ -6,6 +6,7 @@ import {
     Icon,
     Input
 } from "@ui-kitten/components";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const DocHome = ({ navigation }) => {
 
@@ -21,7 +22,7 @@ const DocHome = ({ navigation }) => {
             <ScrollView style={{ flex: 1, backgroundColor: "#fff" }}>
                 <View style={{ padding: 20, paddingTop: 30 }}>
                     {/* Header */}
-                    <View style={{ flexDirection: 'row', alignItems: 'center'}}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                         <Input
                             style={styles.input}
                             value={value}
@@ -31,7 +32,21 @@ const DocHome = ({ navigation }) => {
                         />
                         <Avatar size='small' source={require('../../assets/avatar.jpeg')} />
                     </View>
-                    
+                    <TouchableOpacity onPress={() => navigation.navigate("PatientDetails")}>
+                        <View style={styles.patientCard}>
+                            <View style={{ flexDirection: 'row' }}>
+                                <View style={styles.patientInfo}>
+                                    <Text category='h5'>Patient Name</Text>
+                                    <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                                        <Text style={styles.text}>Blood Type: O+</Text>
+                                        <Text style={styles.text}>Age: 40</Text>
+                                    </View>
+                                    <Text style={styles.text}>Caretaker: Nurse Meghana</Text>
+                                </View>
+                                <Avatar style={styles.avatar} source={require('../../assets/avatar.jpeg')} />
+                            </View>
+                        </View>
+                    </TouchableOpacity>
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -42,6 +57,31 @@ const styles = StyleSheet.create({
     input: {
         flex: 1,
         marginRight: 20,
+        marginBottom: 10
+    },
+    patientCard: {
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 8,
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.2,
+        marginTop: 10
+    },
+    patientInfo: {
+        paddingTop: 5,
+        flex: 1,
+    },
+    text: {
+        fontSize: 14,
+        lineHeight: 24,
+        flex: 1,
+        fontWeight: '500'
+    },
+    avatar: {
+        height: 86,
+        width: 86,
     }
 });
 
--- components/Doctor/PatientDetails.js
+++ components/Doctor/PatientDetails.js
@@ -1,23 +1,84 @@
 import React from "react";
 import { StyleSheet, SafeAreaView, View, ScrollView } from "react-native";
 import {
-    Text
+    Text, Avatar, Tab, TabView, Icon, Layout
 } from "@ui-kitten/components";
 
 const PatientDetails = ({ navigation }) => {
+    const [selectedIndex, setSelectedIndex] = React.useState(null);
+    const PersonIcon = (props) => (
+        <Icon {...props} name='person-outline' />
+    );
+    
+    const EmailIcon = (props) => (
+        <Icon {...props} name='email-outline' />
+    );
+
+
+    const shouldLoadComponent = (index) => index === selectedIndex;
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <ScrollView style={{ flex: 1, backgroundColor: "#fff" }}>
-                <View style={{ padding: 20 }}>
-                    <Text>DOC home</Text>
+                <View style={styles.headerCard}>
+                    <Avatar style={styles.avatar} source={require('../../assets/avatar.jpeg')} />
+                    <View style={styles.headerTxtCon}>
+                        <Text category='h5' style={{ marginBottom: 10 }}>Patient Name</Text>
+                        <Text style={styles.text}>Blood Type: O+</Text>
+                        <Text style={styles.text}>Age: 40</Text>
+                        <Text style={styles.text}>Caretaker: Nurse Meghana</Text>
+                    </View>
                 </View>
+                <TabView
+                    style={styles.tabView}
+                    selectedIndex={selectedIndex}
+                    shouldLoadComponent={shouldLoadComponent}
+                    onSelect={index => setSelectedIndex(index)}>
+                    <Tab title='Medical' icon={PersonIcon}>
+                        <Layout style={styles.tabContainer}>
+                            <Text category='h5'>ORDERS</Text>
+                        </Layout>
+                    </Tab>
+                    <Tab title='Personal' icon={EmailIcon}>
+                        <Layout style={styles.tabContainer}>
+                            <Text category='h5'>USERS</Text>
+                        </Layout>
+                    </Tab>
+                </TabView>
             </ScrollView>
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-
+    avatar: {
+        width: 110,
+        height: 110
+    },
+    headerTxtCon: {
+        marginLeft: 20
+    },
+    text: {
+        fontSize: 14,
+        lineHeight: 24,
+    },
+    headerCard: {
+        backgroundColor: 'white',
+        padding: 20,
+        flexDirection: 'row', alignItems: 'center',
+        borderRadius: 8,
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.2,
+    },
+    tabView: {
+        backgroundColor: 'white'
+    },
+    tabContainer: {
+        padding: 20,
+        backgroundColor: 'white'
+    }
 });
 
 export default PatientDetails;
