1ca586ed1c9f23aa99abcc423baf04e0023d0c9e
Varun V <56495025+created-by-varun@users.noreply.github.com>
2022-03-10 08:22:23 +0530

    media url change

--- components/CareTaker/Home.js
+++ components/CareTaker/Home.js
@@ -1,25 +1,65 @@
 import React from "react";
-import {
-    StyleSheet,
-    SafeAreaView,
-    View,
-    ScrollView,
-    TouchableWithoutFeedback,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
 import Home from "../Doctor/Home";
+import { Button, Modal, Input, Card } from "@ui-kitten/components";
+import { Appwrite } from "appwrite";
+
+const appwrite = new Appwrite();
+
+appwrite
+    .setEndpoint("http://localhost/v1") // Your Appwrite Endpoint
+    .setProject("6228cca7c05ffbaf27d3"); // Your project ID
+
 
 const CareHome = ({ navigation }) => {
+    const [visible, setVisible] = React.useState(false);
+    const [link, setlink] = React.useState(false);
+
+    const addLink = () => {
+        let addLinkRequest = appwrite.database.updateDocument('6228d54f76210a45d192',
+            "6228d69ee8b81276abfb",
+            {
+                musicPlaylist: link,
+            }
+        );
+        addLinkRequest.then(
+            function (response) {
+               console.log("success");
+               setVisible(false);
+            },
+            function (error) {
+                console.log(error); // Failure
+            }
+        );
+    };
+
     return (
         <>
             <Home navigation={navigation} caretaker={true}></Home>
+            <View style={{ padding: 20, backgroundColor: "#fff" }}>
+                <Button onPress={() => setVisible(true)}>Add Music Link</Button>
+            </View>
+            <Modal
+                visible={visible}
+                backdropStyle={styles.backdrop}
+                onBackdropPress={() => setVisible(false)}
+            >
+                <Card disabled={true} style={{ width: 260 }}>
+                    <Input
+                        style={styles.input}
+                        onChangeText={setlink}
+                        value={link}
+                        placeholder="Enter Music Link"
+                    />
+                    <Button onPress={() => addLink()}>ADD</Button>
+                </Card>
+            </Modal>
         </>
     );
 };
 
 const styles = StyleSheet.create({
     input: {
-        flex: 1,
-        marginRight: 20,
         marginBottom: 10,
     },
     patientCard: {
@@ -45,6 +85,9 @@ const styles = StyleSheet.create({
     avatar: {
         height: 86,
         width: 86,
+    },
+    backdrop: {
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
     },
 });
 
--- components/OtherUI/Music.js
+++ components/OtherUI/Music.js
@@ -1,20 +1,70 @@
-import React from "react";
-import { WebView } from 'react-native-webview';
+import React, { useEffect, useState } from "react";
+import { WebView } from "react-native-webview";
 import { Text } from "@ui-kitten/components";
 import { View } from "react-native";
+import { Appwrite } from "appwrite";
+import { useFocusEffect } from "@react-navigation/native";
 
-const MusicScreen = () => {
+const appwrite = new Appwrite();
+
+appwrite
+    .setEndpoint("http://localhost/v1") // Your Appwrite Endpoint
+    .setProject("6228cca7c05ffbaf27d3"); // Your project ID
+
+appwrite.account.createAnonymousSession().then(
+    response => {
+        console.log(response); // Success
+    },
+    error => {
+        console.log(error); // Failure
+    }
+);
+
+let getUserRequest = appwrite.database.listDocuments("6228d54f76210a45d192");
+
+const MusicScreen = ({navigation}) => {
+    const [user, setUser] = React.useState({});
+    // useEffect(() => {
+    //     getUserRequest.then(
+    //         function (response) {
+    //             setUser(response.documents[0]);
+    //         },
+    //         function (error) {
+    //             console.log(error); // Failure
+    //         }
+    //     );
+    // }, []);
+
+    useFocusEffect(
+        React.useCallback(() => {
+            getUserRequest.then(
+                function (response) {
+                    setUser(response.documents[0]);
+                },
+                function (error) {
+                    console.log(error); // Failure
+                }
+            );
+        }, [navigation])
+    );
+
     return (
-      <>
-        <View style={{padding: 20, backgroundColor: 'white'}}>
-            <Text category={'h4'} style={{width: '80%'}}>Top calming music picks for today</Text>
-        </View>
-          <WebView 
-          style={{flex: 1}}
-          source={{ uri: 'https://wynk.in/music/album/patient-heart/um_00602508252372' }}
-          />
-      </>
-    )
-}
+        <>
+            <View style={{ padding: 20, backgroundColor: "white" }}>
+                <Text category={"h4"} style={{ width: "80%" }}>
+                    Top calming music picks for today
+                </Text>
+            </View>
+            <WebView
+                style={{ flex: 1 }}
+                source={{
+                    uri:
+                        user?.musicPlaylist ||
+                        "https://wynk.in/music/album/patient-heart/um_00602508252372",
+                }}
+            />
+        </>
+    );
+};
 
-export default MusicScreen
+export default MusicScreen;
